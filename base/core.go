f1a5a649f8f4ffbe1d52f0232bf917d98081f816
Alter <tuuchan0420@gmail.com>
2024-12-09 01:15:08 +0900

    会計か判定入れた

--- base/core.go
+++ base/core.go
@@ -1,6 +1,7 @@
 package base
 
 import (
+	"context"
 	"log"
 	"strings"
 	"time"
@@ -11,18 +12,40 @@ import (
 func (b *base) BotHandler() {
 	log.Println(time.Now())
 	b.bot.OnPing(b.Ping)
-	b.bot.OnDirectMessageCreated(DMCreated)
+	b.bot.OnDirectMessageCreated(b.DMCreated)
 }
 
-func DMCreated(p *payload.DirectMessageCreated) {
+func (b *base) DMCreated(p *payload.DirectMessageCreated) {
 	quoted := false
+	if b.OnGroupExists(p.Message.User.ID, "ba6552f8-cd46-4123-803b-89440da06860") {
+		log.Println("会計じゃないのはダメ!!")
+	}
+	// 会計でない人からのDMの場合無視
+	// if(b.bot.API().GroupApi)
 	// if there are quotation,ignore space
 	sep := strings.FieldsFunc(p.Message.Text, func(r rune) bool {
 		if r == '"' {
 			quoted = !quoted
 		}
-		return !quoted && r == ' '
+		return !quoted && (r == ' ' || r == '\n')
 	})
 
-	log.Println(sep) // Foo, bar, random, "letters lol", stuff
+	if sep[0] != "/sendTo" {
+		return
+	}
+	sendlist := strings.FieldsFunc(sep[1], func(r rune) bool { return r == ',' })
+	// 全員が存在するIDでなければDM送信しない
+
+	log.Println(sep)      // Foo, bar, random, "letters lol", stuff
+	log.Println(sendlist) // Foo, bar, random, "letters lol", stuff
+}
+
+func (b *base) OnGroupExists(userID string, groupID string) bool {
+	group, _, _ := b.bot.API().GroupApi.GetUserGroup(context.Background(), groupID).Execute()
+	for _, m := range group.Members {
+		if userID == m.Id {
+			return true
+		}
+	}
+	return false
 }
